a0efdc49db716ff4a9371f7f5dbe562a23064dec
shyyko.serhiy@gmail.com <shyyko.serhiy@gmail.com>
2015-12-08 01:03:12 +0200

    promisified queryStatus and added isRepeating and isShuffling quering

--- src/SpotifyStatusController.ts
+++ src/SpotifyStatusController.ts
@@ -6,11 +6,14 @@ export class SpotifyStatusController {
     private _timeoutId: number;
 
     constructor(spotifyStatus: SpotifyStatus) {
+        console.log('dsad', 'asdas');
+        this._timeoutId = null;
         this._spotifyStatus = spotifyStatus;
         this.queryStatus();
     }
 
     scheduleQueryStatus() {
+        this._clearQueryTimeout();
         this._timeoutId = setTimeout(() => {
             this.queryStatus();
         }, 1000);
@@ -19,47 +22,106 @@ export class SpotifyStatusController {
     /**
      * Retrieves status of spotify and passes it to spotifyStatus;
      */
-    queryStatus() {
-        spotify.isRunning((err, isRunning) => {
-            if (isRunning) {
-                spotify.getState((err, state) => {
-                    if (state) {
-                        spotify.getTrack((err, track) => {
-                            if (track) {
-                                this._spotifyStatus.state = {
-                                    state: state,
-                                    track: track,
-                                    isRunning: isRunning
-                                }                                                                
-                            } else {
-                                if (err){
-                                    console.error(err, track);
-                                }
-                            }
-                            this.scheduleQueryStatus();
-                        });
-                    } else {
-                        if (err){
-                            console.error(err, state);
-                        }
-                        this.scheduleQueryStatus();
-                    }
-                });
-            } else {
+    queryStatus() {   
+        this._clearQueryTimeout();    
+        var clearState = ((err: any) => {
+            this._spotifyStatus.state = {
+                state: null,
+                track: null,
+                isRepeating: false,
+                isShuffling: false,
+                isRunning: false
+            };
+            this.scheduleQueryStatus();
+        });
+
+        this._promiseIsRunning().then((isRunning) => {
+            Promise.all<spotify.State | spotify.Track | boolean>([
+                this._promiseGetState(),
+                this._promiseGetTrack(),
+                this._promiseIsRepeating(),
+                this._promiseIsShuffling()
+            ]).then((values) => {
                 this._spotifyStatus.state = {
-                    state: null,
-                    track: null,
-                    isRunning: false
-                }
-                if (err){
-                    console.error(err, isRunning);
+                    state: values[0] as spotify.State,
+                    track: values[1] as spotify.Track,
+                    isRepeating: values[2] as boolean,
+                    isShuffling: values[3] as boolean,
+                    isRunning: true
                 }
                 this.scheduleQueryStatus();
-            }
-        });
+            }).catch(clearState);
+        }).catch(clearState);
     }
 
     dispose() {
         clearTimeout(this._timeoutId);
+    }
+    
+    private _clearQueryTimeout(){
+         if (this._timeoutId !== null){
+            clearTimeout(this._timeoutId);
+            this._timeoutId = null;
+        }
+    }
+
+    private _promiseIsRunning() {
+        return new Promise<boolean>((resolve, reject) => {
+            spotify.isRunning((err, isRunning) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(isRunning);
+                }
+            });
+        });
+    }
+
+    private _promiseGetState() {
+        return new Promise<spotify.State>((resolve, reject) => {
+            spotify.getState((err, state) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(state);
+                }
+            });
+        });
+    }
+
+    private _promiseGetTrack() {
+        return new Promise<spotify.Track>((resolve, reject) => {
+            spotify.getTrack((err, track) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(track);
+                }
+            });
+        });
+    }
+
+    private _promiseIsRepeating() {
+        return new Promise<boolean>((resolve, reject) => {
+            spotify.isRepeating((err, repeating) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(repeating);
+                }
+            });
+        });
+    }
+
+    private _promiseIsShuffling() {
+        return new Promise<boolean>((resolve, reject) => {
+            spotify.isShuffling((err, shuffling) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(shuffling);
+                }
+            });
+        });
     }
 }
