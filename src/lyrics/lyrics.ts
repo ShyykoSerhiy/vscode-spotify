51ec06fa7ac519d0af0e1933a8d729b5c25b59d9
shyyko.serhiy <shyyko.serhiy@gmail.com>
2020-09-28 00:09:41 -0400

    fix: Lyrics functionality doesn't work #133

--- src/lyrics/lyrics.ts
+++ src/lyrics/lyrics.ts
@@ -1,9 +1,10 @@
-import { Event, EventEmitter, ProgressLocation, TextDocumentContentProvider, Uri, window, workspace } from 'vscode';
+import { Event, EventEmitter, ProgressLocation, TextDocumentContentProvider, Uri, window, workspace, QuickPickItem, env } from 'vscode';
 
 import { getLyricsServerUrl, openPanelLyrics } from '../config/spotify-config';
 import { showInformationMessage } from '../info/info';
 import { xhr } from '../request/request';
 import { getState } from '../store/store';
+import * as cheerio from 'cheerio';
 
 class TextContentProvider implements TextDocumentContentProvider {
     htmlContent = '';
@@ -23,6 +24,20 @@ class TextContentProvider implements TextDocumentContentProvider {
     }
 }
 
+interface Song {
+    artist: string,
+    title: string,
+    geniusPath?: string,
+    /**
+     * String similarity score for the song. 
+     */
+    similarity?: number
+}
+
+type V3SongsResponse = {
+    songs?: Song[]
+}
+
 export class LyricsController {
     private static lyricsContentProvider = new TextContentProvider();
 
@@ -32,7 +47,7 @@ export class LyricsController {
 
     async findLyrics() {
         window.withProgress({ location: ProgressLocation.Window, title: 'Searching for lyrics. This might take a while.' }, () =>
-        this._findLyrics());
+            this._findLyrics());
     }
 
     private async _findLyrics() {
@@ -40,10 +55,61 @@ export class LyricsController {
         const { artist, name } = state.track;
 
         try {
+            const url = `${getLyricsServerUrl()}?artist=${encodeURIComponent(artist)}&title=${encodeURIComponent(name)}`;
             const result = await xhr({
-                url: `${getLyricsServerUrl()}?artist=${encodeURIComponent(artist)}&title=${encodeURIComponent(name)}`
+                url
             });
-            await this._previewLyrics(`${artist} - ${name}\n\n${result.responseText.trim()}`);
+
+            const { songs } = JSON.parse(result.responseText) as V3SongsResponse;
+
+            if (!songs || !songs.length) {
+                await this._previewLyrics(`Song lyrics for ${artist} - ${name} not found.\nYou can add it on https://genius.com/ .`);
+                return;
+            }
+
+            let song = songs[0];
+            if (song.similarity !== 1) {
+                type QuickPickItemSong = QuickPickItem & { song: Song };
+                const pick = await window.showQuickPick(
+                    Array.from(songs.map((s): QuickPickItemSong => {
+                        return {
+                            label: `${s.artist} - ${s.title}`,
+                            song: s
+                        };
+                    })),
+                    {
+                        ignoreFocusOut: true,
+                        placeHolder: 'Select one of the songs that we think might be your song.'
+                    }
+                );
+                if (!pick) {
+                    return;
+                }
+                song = pick.song;
+            }
+
+            const geniusUrl = `https://genius.com${song.geniusPath}`;
+            try {
+                const fetchRes = await xhr({
+                    url: geniusUrl
+                });
+                const $ = cheerio.load(fetchRes.responseText);
+                const lyrics = $('.lyrics').text().trim();
+                await this._previewLyrics(`${artist} - ${name}\n\n${lyrics}`);
+            }
+            catch (e) {
+                if (e.status === 403) {
+                    // probably captcha. Open in browser
+                    await env.openExternal(Uri.parse(geniusUrl));
+                    await this._previewLyrics(`Song lyrics for ${artist} - ${name} not found.\nYou can add it on https://genius.com/ .`);
+                }
+                if (e.status === 404) {
+                    await this._previewLyrics(`Song lyrics for ${artist} - ${name} not found.\nYou can add it on https://genius.com/ .`);
+                }
+                if (e.status === 500) {
+                    await this._previewLyrics(`Error: ${e.responseText}`);
+                }
+            }
         } catch (e) {
             if (e.status === 404) {
                 await this._previewLyrics(`Song lyrics for ${artist} - ${name} not found.\nYou can add it on https://genius.com/ .`);
