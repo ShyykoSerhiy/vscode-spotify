9854f0bfab7d091f74f41bc3f1fa537596fe8acc
Lorenze Jay Hernandez <lorenzejaytech@gmail.com>
2022-04-03 20:23:09 -0700

    removed prettier changes and removed unused additional packages that were added for the extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,14 @@
-import { ExtensionContext, window } from "vscode";
-import * as vscode from "vscode";
-import { createCommands } from "./commands";
-import { SpotifyStatus } from "./components/spotify-status";
-import {
-  connectPlaylistTreeView,
-  TreePlaylistProvider,
-} from "./components/tree-playlists";
-import {
-  connectAlbumTreeView,
-  TreeAlbumProvider,
-} from "./components/tree-albums";
-import {
-  connectTrackTreeView,
-  TreeTrackProvider,
-} from "./components/tree-track";
-import { registerGlobalState } from "./config/spotify-config";
-import { SpotifyStatusController } from "./spotify-status-controller";
-import { SpoifyClientSingleton } from "./spotify/spotify-client";
-import { getStore } from "./store/store";
+import { ExtensionContext, window } from 'vscode';
+
+import { createCommands } from './commands';
+import { SpotifyStatus } from './components/spotify-status';
+import { connectPlaylistTreeView, TreePlaylistProvider } from './components/tree-playlists';
+import { connectAlbumTreeView, TreeAlbumProvider } from './components/tree-albums';
+import { connectTrackTreeView, TreeTrackProvider } from './components/tree-track';
+import { registerGlobalState } from './config/spotify-config';
+import { SpotifyStatusController } from './spotify-status-controller';
+import { SpoifyClientSingleton } from './spotify/spotify-client';
+import { getStore } from './store/store';
 import type { Client } from "tangle";
 import { ILoginState, IPlayerState, ITrack } from "./state/state";
 import { getState } from "./store/store";
@@ -26,23 +17,13 @@ import { getState } from "./store/store";
 // controlled by the activation events defined in package.json.
 export function activate(context: ExtensionContext) {
   // This line of code will only be executed once when your extension is activated.
-  registerGlobalState(context.globalState);
-  getStore(context.globalState).getState();
-
   const spotifyStatus = new SpotifyStatus();
   const controller = new SpotifyStatusController();
-  const playlistTreeView = window.createTreeView("vscode-spotify-playlists", {
-    treeDataProvider: new TreePlaylistProvider(),
-  });
-  const albumTreeView = window.createTreeView("vscode-spotify-albums", {
-    treeDataProvider: new TreeAlbumProvider(),
-  });
+  const playlistTreeView = window.createTreeView('vscode-spotify-playlists', { treeDataProvider: new TreePlaylistProvider() });
+  const albumTreeView = window.createTreeView('vscode-spotify-albums', { treeDataProvider: new TreeAlbumProvider() });
   const treeTrackProvider = new TreeTrackProvider();
-  const trackTreeView = window.createTreeView("vscode-spotify-tracks", {
-    treeDataProvider: treeTrackProvider,
-  });
+  const trackTreeView = window.createTreeView('vscode-spotify-tracks', { treeDataProvider: treeTrackProvider });
   treeTrackProvider.bindView(trackTreeView);
-
   // Add to a list of disposables which are disposed when this extension is deactivated.
   context.subscriptions.push(connectPlaylistTreeView(playlistTreeView));
   context.subscriptions.push(connectAlbumTreeView(albumTreeView));
@@ -50,9 +31,7 @@ export function activate(context: ExtensionContext) {
   context.subscriptions.push(controller);
   context.subscriptions.push(spotifyStatus);
   context.subscriptions.push(playlistTreeView);
-  context.subscriptions.push(
-    createCommands(SpoifyClientSingleton.spotifyClient)
-  );
+  context.subscriptions.push(createCommands(SpoifyClientSingleton.spotifyClient));
 
   return {
     marquee: {
@@ -63,9 +42,8 @@ export function activate(context: ExtensionContext) {
           loginState: ILoginState;
         }>
       ) => {
-        getStore().subscribe(() => {
-          const { track, playerState, loginState } = getState();
-          tangle.emit("loginState", loginState);
+        getStore(context.globalState).subscribe(() => {
+          const { track, playerState } = getState();
           tangle.emit("track", track);
           tangle.emit("playerState", playerState);
         });
